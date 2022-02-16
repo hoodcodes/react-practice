ae9614d41ccb6893441d7ede127df49c42b62192
Christopher Hood <chris@cshood.com>
2022-02-15 18:06:21 -0700

    add NavigationBar

---
+++ src/components/navigationbar.tsx
@@ -0,0 +1,13 @@
+import React  from 'react';
+
+const NavigationBar = () => {
+  return(
+    <div>
+      <p>
+        Navigation Bar
+      </p>
+    </div>
+  )
+}
+
+export default NavigationBar;
